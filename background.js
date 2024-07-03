7fc2cb16a5c13460898211328a0d85ec54b90813
abhijit <kamathabhijit2002@gmail.com>
2024-07-03 15:27:44 -0400

    transition to using react

--- background.js
+++
@@ -1,8 +0,0 @@
-import { INCREMENT_KEY } from "./common"
-async function incrementTimer(){
-    let currentValue = await chrome.storage.sync.get(INCREMENT_KEY)
-    currentValue[INCREMENT_KEY] = Date.now - currentValue[INCREMENT_KEY]
-    await chrome.storage.sync.set(currentValue)
-}
-
-incrementTimer()
