93de293bce934aee5c2372793b7c65a8985ce886
abhijit <kamathabhijit2002@gmail.com>
2024-07-05 21:31:58 -0400

    create task component

--- src/components/task-display/task.tsx
+++ src/components/task-display/task.tsx
@@ -15,20 +15,30 @@ export function Task({ taskName, startTime, endTime, attempted }: TaskProps) {
   }
   return (
     <>
-      <div>
-        <p>{taskName}</p>
+      <div className="task-container">
+        <p className="task-name">{taskName}</p>
         {attempted ? (
-          <p>{formatTime(new Date(endTime.getTime() - startTime.getTime()))}</p>
+          <p className="task-date">
+            {formatTime(new Date(endTime.getTime() - startTime.getTime()))}
+          </p>
         ) : (
-          <p>
+          <p className="task-date">
             `${formatTime(startTime)} - ${formatTime(endTime)}`
           </p>
         )}
-        <img src="./check.svg" style={{ width:"24px", height:"24px"}} />
-        <img src="./delete.svg" style={{ width:"24px", height:"24px"}}/>
-        <img src="edit.svg"style={{ width:"24px", height:"24px"}} />
+        <div className="task-utilities">
+          {attempted ? (
+            <>
+              <img src="edit.svg" alt="continue or edit time entry" style={{ width: "24px", height: "24px", cursor: "pointer" }} />
+            </>
+          ) : (
+            <>
+              <img src="play.svg" alt="start time entry" style={{ width: "24px", height: "24px", cursor: "pointer"}} />
+            </>
+          )}
+            <img src="./delete.svg" alt="delete time entry" style={{ width: "24px", height: "24px", cursor: "pointer" }} />
+        </div>
       </div>
-      
     </>
   );
 }
