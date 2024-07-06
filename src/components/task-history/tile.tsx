a5afddfb949c7983ac13a2027099e19d653fa98a
abhijit <kamathabhijit2002@gmail.com>
2024-07-06 15:44:12 -0400

    finished up tooltip design

--- src/components/task-history/tile.tsx
+++ src/components/task-history/tile.tsx
@@ -11,15 +11,28 @@ export function Tile({ completedTasks, totalTasks, day }: TileProps) {
   const [visibility, setVisibility] = useState<boolean>(false);
   const tasksCompletedRatio = completedTasks / totalTasks;
 
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseEnter= (e: React.MouseEvent) => {
+      setPosition({
+          x: e.clientX - 10,
+          y: e.clientY - 20
+      });
+      setVisibility(true);
+  };
+
+  const handleMouseOut = () => {
+      setVisibility(false);
+  };
   return (
     <>
       <span
         className={`tile `+` ${tasksCompletedRatio === 0 ? "" : tasksCompletedRatio > 0.9 ? `green4` : tasksCompletedRatio > 0.7 ? 'green3' : tasksCompletedRatio > 0.4 ? 'green2' : 'green1'}`}
-        onMouseEnter={() => setVisibility(true)}
-        onMouseLeave={() => setVisibility(false)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseOut}
       >{day}</span>
       {visibility && (
-        <span className="tile-tooltip">{totalTasks} tasks completed</span>
+        <span className="tile-tooltip" style={{left: position.x , top: position.y}}>{completedTasks} tasks completed</span>
       )}
     </>
   );
