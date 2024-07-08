220517fef3d2409eeeb621abb7afe4133aa51811
abhijit <kamathabhijit2002@gmail.com>
2024-07-08 09:13:09 -0400

    react-hook-form to edit task data initial commit

--- src/components/task-editor/task-editor.tsx
+++ src/components/task-editor/task-editor.tsx
@@ -5,16 +5,39 @@ import {useForm, Resolver} from "react-hook-form";
 type FormValues = {
     "title": string
     "body": string
-    "start-time": [Date],
-    "end-time": [Date],
+    "times": {"startTime":Date, "endTime": Date}[],
     "state": "completed" | "in-progress" | "started"
 }
 
+const resolver: Resolver<FormValues> = async (values) => {
+    return {
+        values: values.title ? values : {},
+        errors: !values.title ? {
+            title: {
+                type:"required",
+                message: "Title field is required.",
+            },
+        } : {},
+    }
+}
 
-export function TaskEditor() {
+export function TaskEditor({TaskData}:{ TaskData: FormValues}) {
+    const { register, handleSubmit, formState: {errors},} = useForm<FormValues>({resolver})
+    const onSubmit = handleSubmit((data) => console.log(data))
+    
+
   return (
     <>
       <div className="task-editor-container">
+        <form onSubmit={onSubmit}>
+            <input {...register("title"), {required: true}} placeholder="Task Title" className="Task-Title"/>
+            {errors?.title && <p>{errors.title.message}</p>}
+
+            <input {...register("body"), {required: true}} placeholder="Task Body" className="Task-body"/>
+
+            <input type="submit" />
+        </form>
+    
       </div>
     </>
   );
