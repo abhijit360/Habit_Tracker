4c04f65f7d84920ce4ba89f29d9c68f8242942d7
abhijit <kamathabhijit2002@gmail.com>
2024-07-14 13:43:11 -0400

    getting Oauth data

--- src/containers/components/auth/login.tsx
+++ src/containers/components/auth/login.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import type { GoogleUserObj } from '../../../../types';
+
 export function LogIn() {
+  const [tokenAvailability, setTokenAvailability] = useState<boolean>(false);
+  const [userProfile, setUserProfile] = useState<GoogleUserObj>({} as GoogleUserObj)
+  async function checkExistingToken() {
+    const response = await chrome.storage.session.get(
+      'lockIn-curr-google-token'
+    );
+    if (response['lockIn-curr-google-token']) {
+      console.log(response);
+      setTokenAvailability(true);
+      const profile_data = await (await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${response['lockIn-curr-google-token']}`)).json()
+      setUserProfile(profile_data)
+    }
+  }
+  useEffect(() => {
+    checkExistingToken();
+  },[]);
   function loginHandler(e: React.MouseEvent) {
     if (chrome.identity) {
       chrome.identity.getAuthToken({ interactive: true }, function (token) {
         if (chrome.runtime.lastError) {
           console.error(chrome.runtime.lastError);
         } else {
-          console.log(token);
+          chrome.storage.session.set({ 'lockIn-curr-google-token': token });
         }
       });
     } else {
       console.error('chrome.identity is not available.');
     }
   }
+  console.log(tokenAvailability)
   return (
     <>
       <div className="Login-form-container">
         <p className="login-form-description"></p>
-        <button className="login-form-button" onClick={loginHandler}>
-          Login
-        </button>
+        {!tokenAvailability ? (
+          <>
+            <button className="login-form-button" onClick={loginHandler}>
+              Login
+            </button>
+          </>
+        ) : (
+          <>
+          <p>welcome {userProfile.given_name}</p>
+          <p>Do you want to import today's google calendar events?</p>
+          <button>Import</button>
+          <button>Skip</button>
+          </>
+        )}
       </div>
     </>
   );
--- types.ts
+++ types.ts
@@ -6,3 +6,12 @@ export type TaskType = {
     state: "completed" | "in-progress" | "new";
   };
   
+export type GoogleUserObj = {
+  sub: string;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  email: string;
+  email_verified: boolean
+}
