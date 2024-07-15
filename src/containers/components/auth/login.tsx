d5d65c20a7225ec8939ce9942a8fc63a46099b1c
abhijit <kamathabhijit2002@gmail.com>
2024-07-15 14:30:54 -0400

    improving login UI with better management of loggedIn state

--- src/containers/components/auth/login.tsx
+++ src/containers/components/auth/login.tsx
@@ -3,35 +3,53 @@ import type { GoogleUserObj } from '../../../../types';
 
 export function LogIn() {
   const [tokenAvailability, setTokenAvailability] = useState<boolean>(false);
-  const [userProfile, setUserProfile] = useState<GoogleUserObj>({} as GoogleUserObj)
+  const [userProfile, setUserProfile] = useState<GoogleUserObj>(
+    {} as GoogleUserObj
+  );
   async function checkExistingToken() {
     const response = await chrome.storage.session.get(
       'lockIn-curr-google-token'
     );
     if (response['lockIn-curr-google-token']) {
-      console.log(response);
+      console.log(response['lockIn-curr-google-token']);
       setTokenAvailability(true);
-      const profile_data = await (await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${response['lockIn-curr-google-token']}`)).json()
-      setUserProfile(profile_data)
+      const profile_data = await (
+        await fetch(
+          `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${response['lockIn-curr-google-token']}`
+        )
+      ).json();
+      setUserProfile(profile_data);
     }
   }
   useEffect(() => {
     checkExistingToken();
-  },[]);
-  function loginHandler(e: React.MouseEvent) {
+  }, [tokenAvailability]);
+
+  async function loginHandler(e: React.MouseEvent) {
     if (chrome.identity) {
-      chrome.identity.getAuthToken({ interactive: true }, function (token) {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError);
-        } else {
-          chrome.storage.session.set({ 'lockIn-curr-google-token': token });
+      chrome.identity.getAuthToken(
+        { interactive: true },
+        async function (token) {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          } else {
+            await chrome.storage.session.set({
+              'lockIn-curr-google-token': token,
+            });
+            setTokenAvailability(true);
+          }
         }
-      });
+      );
     } else {
       console.error('chrome.identity is not available.');
     }
   }
-  console.log(tokenAvailability)
+
+  function logoutHandler() {
+    chrome.storage.session.set({ 'lockIn-curr-google-token': null });
+    setUserProfile({} as GoogleUserObj);
+    setTokenAvailability(false);
+  }
   return (
     <>
       <div className="Login-form-container">
@@ -44,10 +62,11 @@ export function LogIn() {
           </>
         ) : (
           <>
-          <p>welcome {userProfile.given_name}</p>
-          <p>Do you want to import today's google calendar events?</p>
-          <button>Import</button>
-          <button>Skip</button>
+            <p>welcome {userProfile.given_name}</p>
+            <p>Do you want to import today's google calendar events?</p>
+            <button>Import</button>
+            <button>Skip</button>
+            <button onClick={logoutHandler}>LogOut</button>
           </>
         )}
       </div>
