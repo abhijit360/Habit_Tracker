fdc188d3eb31062442482e465ce9ea9345185c92
abhijit <kamathabhijit2002@gmail.com>
2024-07-17 22:19:01 -0400

    added navigational features into component

--- stores/navigationStore.ts
+++ stores/navigationStore.ts
@@ -5,6 +5,7 @@ interface NavigationState {
   prev_navigation_states: Pages[];
   current_navigation_state: Pages;
   updateNavigation: (page: Pages) => void;
+  revertToPreviousState: () => void;
 }
 
 export const useNavigationStore = create<NavigationState>((set) => ({
@@ -19,5 +20,16 @@ export const useNavigationStore = create<NavigationState>((set) => ({
           ? [page, ...state.prev_navigation_states.slice(0, 4)]
           : [page, ...state.prev_navigation_states],
     }));
+  },
+  revertToPreviousState: () => {
+    set((state) => {
+      if (state.prev_navigation_states.length === 0) {
+        return state; // No previous state to revert to
+      }
+      return {
+        current_navigation_state: state.prev_navigation_states[0],
+        prev_navigation_states: state.prev_navigation_states.slice(1),
+      };
+    });
   },
 }));
