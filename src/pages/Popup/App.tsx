17d1a6c73029bfca457ca6ca865d5e94701cd6fa
abhijit <kamathabhijit2002@gmail.com>
2024-07-28 17:01:28 -0400

    adding branding and credentials

--- src/pages/Popup/App.tsx
+++ src/pages/Popup/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useCallback, useEffect } from 'react';
 import { TaskDisplay } from '../../containers/components/task-display/task-display';
-import { TaskHistory } from '../../containers/components/task-history/task-history';
 import { TaskTracker } from '../../containers/components/task-time-tracker/task-tracker';
 import { TaskEditor } from '../../containers/components/task-editor/task-editor';
 import { LogIn } from '../../containers/components/auth/login';
@@ -10,7 +9,7 @@ import { useNavigationStore } from '../../../stores/navigationStore';
 import { useErrorStore } from '../../../stores/errorStore';
 import type { TaskType, Page } from '../../../types';
 import { useTasksStore } from '../../../stores/taskStore';
-
+import githubIcon from '../../assets/img/github-mark-white.svg';
 
 function App() {
   const {
@@ -21,7 +20,7 @@ function App() {
     updateCurrentTask,
   } = useNavigationStore();
   const { error, removeError } = useErrorStore();
-  const {tasks, clearTaskState} = useTasksStore()
+  const { tasks, clearTaskState } = useTasksStore();
 
   function incrementNavigation(e: React.MouseEvent) {
     const target = e.target as HTMLButtonElement;
@@ -35,22 +34,24 @@ function App() {
   }, [error, removeError]);
 
   const checkForPeristingTaskState = useCallback(async () => {
-    const authToken = await chrome.storage.session.get('lockIn-curr-google-token');
-    if(!authToken['lockIn-curr-google-token']){
-      return 
+    const authToken = await chrome.storage.session.get(
+      'lockIn-curr-google-token'
+    );
+    if (!authToken['lockIn-curr-google-token']) {
+      return;
     }
     const task_id_obj = await chrome.storage.session.get('current-task-id');
     if (task_id_obj['current-task-id']) {
       updateCurrentTask(task_id_obj['current-task-id']);
     }
-  },[updateCurrentTask]);
+  }, [updateCurrentTask]);
 
-  async function handleLogOut(){
+  async function handleLogOut() {
     await chrome.storage.session.set({ 'lockIn-curr-google-token': null });
-    clearTaskState()
-    updateNavigation("Login")
+    clearTaskState();
+    updateNavigation('Login');
     // eslint-disable-next-line no-restricted-globals
-    location.reload()
+    location.reload();
   }
 
   useEffect(() => {
@@ -67,9 +68,13 @@ function App() {
           <button
             className="nav-button"
             value={'Login'}
-            onClick={() => current_navigation_state === "Login" ? handleLogOut() :  updateNavigation("Login") }
+            onClick={() =>
+              current_navigation_state === 'Login'
+                ? handleLogOut()
+                : updateNavigation('Login')
+            }
           >
-            {current_navigation_state !== "Login" ? "Home": "Logout" }
+            {current_navigation_state !== 'Login' ? 'Home' : 'Logout'}
           </button>
           <button
             className="nav-button"
@@ -78,15 +83,15 @@ function App() {
           >
             Task Display
           </button>
-          {current_task_id && 
-          <button
-            className="nav-button"
-            value={'TaskTimer'}
-            onClick={incrementNavigation}
-          >
-            Time Tracker
-          </button>
-        }
+          {current_task_id && (
+            <button
+              className="nav-button"
+              value={'TaskTimer'}
+              onClick={incrementNavigation}
+            >
+              Time Tracker
+            </button>
+          )}
         </div>
         {/* <TaskHistory /> */}
         {current_navigation_state === 'Login' ? (
@@ -94,12 +99,29 @@ function App() {
         ) : current_navigation_state === 'TaskDisplay' ? (
           <TaskDisplay />
         ) : current_navigation_state === 'TaskEdit' ? (
-          <TaskEditor TaskData={current_edit_task_id ? tasks.filter((task) => task.id === current_edit_task_id )[0] : null} state={"edit"}/>
+          <TaskEditor
+            TaskData={
+              current_edit_task_id
+                ? tasks.filter((task) => task.id === current_edit_task_id)[0]
+                : null
+            }
+            state={'edit'}
+          />
         ) : current_navigation_state === 'TaskAdd' ? (
-          <TaskEditor TaskData={{} as TaskType} state={"add"}/>
+          <TaskEditor TaskData={{} as TaskType} state={'add'} />
         ) : (
           <TaskTracker />
         )}
+        <div className="credentials-container">
+          <p>Built by Abhijit Kamath</p>
+          <a href="https://github.com/abhijit360">
+            <img
+              src={githubIcon}
+              style={{ width: '16px', height: '16px' }}
+              alt="My Github"
+            />
+          </a>
+        </div>
       </header>
     </div>
   );
